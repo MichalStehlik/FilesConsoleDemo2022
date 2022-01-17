cd99482f08f0df8e7a02e79d1d010981ab74c730
Michal Stehlík <michal.stehlik@pslib.cz>
2022-01-17 13:19:43 +0100

    text file read and write

--- FilesConsoleDemo2022/Program.cs
+++ FilesConsoleDemo2022/Program.cs
@@ -9,7 +9,7 @@ namespace FilesConsoleDemo2022
         {
             // drives
             string[] drives = Directory.GetLogicalDrives();
-            foreach(string d in drives)
+            foreach (string d in drives)
             {
                 Console.WriteLine(d);
             }
@@ -20,7 +20,7 @@ namespace FilesConsoleDemo2022
             Console.WriteLine(Directory.GetDirectoryRoot(docPath));
             // list
             string[] dirList = Directory.GetDirectories(docPath);
-            foreach(string dir in dirList)
+            foreach (string dir in dirList)
             {
                 Console.WriteLine(dir);
             }
@@ -31,7 +31,7 @@ namespace FilesConsoleDemo2022
                 Console.WriteLine(file);
             }
             Console.WriteLine();
-            string[] dirList2 = Directory.GetDirectories(docPath,"My*",SearchOption.TopDirectoryOnly);
+            string[] dirList2 = Directory.GetDirectories(docPath, "My*", SearchOption.TopDirectoryOnly);
             foreach (string dir in dirList2)
             {
                 Console.WriteLine(dir);
@@ -43,11 +43,55 @@ namespace FilesConsoleDemo2022
             Console.WriteLine("");
             long size = 0;
             var dirInfo = new DirectoryInfo(docPath);
-            foreach(FileInfo fi in dirInfo.GetFiles("*",SearchOption.AllDirectories))
+            foreach (FileInfo fi in dirInfo.GetFiles("*", SearchOption.AllDirectories))
             {
                 size += fi.Length;
             }
             Console.WriteLine(size);
+            string newDir = Path.Combine(docPath, "Special", "Documents", "School");
+            Console.WriteLine(newDir);
+            if (Directory.Exists(newDir))
+            {
+                Console.WriteLine("It exists");
+                // Directory.Delete(newDir);
+            }
+            else
+            {
+                Console.WriteLine("It is not here.");
+                try
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(newDir);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            string newFile = Path.Combine(newDir,"data.txt");
+            StreamWriter sw;
+            if (File.Exists(newFile))
+            {
+                Console.WriteLine("There is such file.");
+                using (sw = File.AppendText(newFile))
+                {
+                    sw.WriteLine("Adding a new line.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ooops");
+                using (sw = File.CreateText(newFile))
+                {
+                    sw.WriteLine("This is new file.");
+                }
+            }
+            using (StreamReader sr = File.OpenText(newFile))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null) {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
