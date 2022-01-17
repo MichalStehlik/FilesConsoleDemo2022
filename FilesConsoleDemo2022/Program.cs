ae4b201afe1b58e52fe779bc8e7cd31a57e6214a
Michal Stehlík <michal.stehlik@pslib.cz>
2022-01-17 10:45:08 +0100

    Add project files.

---
+++ FilesConsoleDemo2022/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace FilesConsoleDemo2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // drives
+            string[] drives = Directory.GetLogicalDrives();
+            foreach(string d in drives)
+            {
+                Console.WriteLine(d);
+            }
+            // directories
+            Console.WriteLine(Directory.GetCurrentDirectory()); // aktuální adresář
+            var docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Console.WriteLine(docPath);
+            Console.WriteLine(Directory.GetDirectoryRoot(docPath));
+            // list
+            string[] dirList = Directory.GetDirectories(docPath);
+            foreach(string dir in dirList)
+            {
+                Console.WriteLine(dir);
+            }
+            Console.WriteLine();
+            string[] fileList = Directory.GetFiles(docPath);
+            foreach (string file in fileList)
+            {
+                Console.WriteLine(file);
+            }
+            Console.WriteLine();
+            string[] dirList2 = Directory.GetDirectories(docPath,"My*",SearchOption.TopDirectoryOnly);
+            foreach (string dir in dirList2)
+            {
+                Console.WriteLine(dir);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Create:" + Directory.GetCreationTime(docPath));
+            Console.WriteLine("Access:" + Directory.GetLastAccessTime(docPath));
+            Console.WriteLine("Write: " + Directory.GetLastWriteTime(docPath));
+            Console.WriteLine("");
+            long size = 0;
+            var dirInfo = new DirectoryInfo(docPath);
+            foreach(FileInfo fi in dirInfo.GetFiles("*",SearchOption.AllDirectories))
+            {
+                size += fi.Length;
+            }
+            Console.WriteLine(size);
+        }
+    }
+}
